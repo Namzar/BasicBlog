d90466234804903a6b34586dd804e1ae90ffde46
Namzar <luc.mainguene@gmail.com>
2016-01-07 17:35:38 +0100

    Create ApplicationType.php

---
+++ src/AppBundle/Form/Type/ApplicationType/ApplicationType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AppBundle\Form\Type\ApplicationType;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ApplicationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $readOnly = $options['read_only'];
+        $builder
+            ->add('name', 'text', array(
+                'read_only' => $readOnly,
+                ))
+            ->add('type', 'text', array(
+                'read_only' => $readOnly,
+                ))
+        ;
+        if (!$readOnly){
+            $builder->add('save', 'submit');
+        }
+            
+    }
+    
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Application',
+            'read_only' => false,
+        ));
+    }
+
+    public function getName()
+    {
+        return 'Application';
+    }
+}
